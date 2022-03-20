e33056c5e165ff7b9e27d5c1804584aa6d589f29
Lucas Garcia <nahuellucasgarcia@gmail.com>
2022-03-20 16:45:38 -0300

    reordered project to deploy react and express in vercel 	https://stackoverflow.com/a/68881566/10773573 	used this answer as a reference

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: __dirname + '/.env' })
+}
+
 //dependencias
 const uri = process.env.MONGODB_URI;
 const PORT_APP = process.env.port || 5000;
@@ -15,3 +19,9 @@ app.listen(PORT_APP, () => {
     console.log("Servidor iniciado en " + PORT_APP);
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+  })
+}
