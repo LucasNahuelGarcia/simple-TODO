c61a8da41e9809cc304fd30dc782b07c6c68528d
Lucas Garcia <nahuellucasgarcia@gmail.com>
2022-03-20 19:18:00 -0300

    Reverted to e33056c5e165ff7b9e27d5c1804584aa6d589f29 	Fixed project for vercel

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 //dependencias
+const path = require("path");
 const uri = process.env.MONGODB_URI;
-const PORT_APP = process.env.port || 5000;
+const PORT_APP = process.env.PORT || 5000;
 
 const morgan = require("morgan");
 const express = require("express");
@@ -19,9 +20,9 @@ app.listen(PORT_APP, () => {
     console.log("Servidor iniciado en " + PORT_APP);
 });
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client', 'build')));
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
   })
-}
+// }
