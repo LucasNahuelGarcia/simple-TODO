6c817fa1605b32c1fe83c577e434fa7bca62d66e
Lucas Garcia <nahuellucasgarcia@gmail.com>
2022-03-20 22:38:01 -0300

    changed default text to show

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ app.listen(PORT_APP, () => {
 // if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client', 'build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+    // res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+    res.send("hola, esta es la pagina");
   })
 // }
